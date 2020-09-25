1c147dcb90249cc3f852afa822c991b133af2b82
Archie Hewitt <hewitt.archie@yandex.ru>
2020-09-25 15:19:07 +0300

    wanna train inception for two_spins

--- floqlind/train_val.py
+++ floqlind/train_val.py
@@ -17,18 +17,18 @@ if __name__ == '__main__':
 
     device = get_device()
 
-    system_train = 'ospm'
+    system_train = 'two_spins'
     size = 16
 
     path_train = get_path() + f'/dl/datasets/floquet_lindbladian/{system_train}'
-    suffix_train = 'ampl(0.2500_0.2500_200)_freq(0.0250_0.0250_200)_phase(0.0000_0.0000_0)'
+    suffix_train = 'ampl(0.5000_0.5000_200)_freq(0.0500_0.0500_200)_phase(0.0000_0.0000_0)'
 
     is_2D = False
 
     function = 'log'
 
     # Models to choose from [resnet, vgg, densenet, inception, resnet50_2D]
-    model_name = "resnet"
+    model_name = "inception"
     model_dir = f'{path_train}/{model_name}_{function}_{suffix_train}'
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
